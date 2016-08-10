6ab2e93d0c39870c0f4497ff020af0dcab73ad1e
Moiz.Kachwala <moizk@hotmail.com>
2016-08-10 14:39:28 +0530

    fixed typos

--- app/db/index.js
+++ app/db/index.js
@@ -3,21 +3,22 @@ const config = require('../config');
 const logger = require('../logger');
 const Mongoose = require('mongoose').connect(config.dbURI);
 
+// Log an error if the connection fails
 Mongoose.connection.on('error', error => {
-    logger.log('error','Mongoose connection error ' + error);
+	logger.log('error', 'Mongoose connection error: ' + error);
 });
 
-//create a schema that defines the structure for storing user data.
+// Create a Schema that defines the structure for storing user data
 const chatUser = new Mongoose.Schema({
-    profileId: String,
-    fullName: String,
-    profilePic: String
+	profileId: String,
+	fullName: String,
+	profilePic: String
 });
 
-// Turn the schema to model
+// Turn the schema into a usable model
 let userModel = Mongoose.model('chatUser', chatUser);
 
 module.exports = {
-    Mongoose,
-    userModel
+	Mongoose,
+	userModel
 }
--- app/logger/index.js
+++ app/logger/index.js
@@ -2,19 +2,14 @@
 
 const winston = require('winston');
 const logger = new (winston.Logger)({
-    transports:[
-        new (winston.transports.File)({
-            level: 'debug',
-            filename: './chatCatDebug.log',
-            handleExceptions: true
-        }),
-        new (winston.transports.Console)({
-            level: 'debug',
-            json: true,
-            handleExceptions: true
-        })
-    ],
-    exitOnError: false
+	transports: [
+		new (winston.transports.Console)({
+			level: 'debug',
+			json: true,
+			handleExceptions: true
+		})
+	],
+	exitOnError: false
 });
 
 module.exports = logger;
